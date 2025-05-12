0767000d21ed3cd446cc1f269b0df43a92534f80
Bibek Saini <itsbibeksaini@gmail.com>
2025-05-11 23:36:24 -0400

    feat: side bar concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import { Grid } from '@mui/material'
+import './App.scss'
+import SideBar from './components/SideBar/SideBar'
 
 function App() {
   return (
-    <Grid>
-      
+    <Grid className='resume-builder max-supported-width' container>
+      <SideBar></SideBar>      
     </Grid>
   )
 }
