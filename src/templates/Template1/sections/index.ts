8494526c478aa5ba6dddf0a9af03042a209b5f58
Bibek Saini <itsbibeksaini@gmail.com>
2025-07-22 14:43:52 -0400

    removing old project

--- src/templates/Template1/sections/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ContactInformation } from './ContactInformation/ContactInformation';
-export { default as Skills } from './Skills/Skills';
-export { default as Education } from './Education/Education';
