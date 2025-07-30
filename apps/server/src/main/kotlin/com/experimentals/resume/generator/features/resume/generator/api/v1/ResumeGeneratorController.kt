ee3eac8846e8c8da638b51ec4fc9e2cc1c3cefdd
Bibek Saini <itsbibeksaini@gmail.com>
2025-07-30 17:51:08 -0400

    rest endpoint

---
+++ apps/server/src/main/kotlin/com/experimentals/resume/generator/features/resume/generator/api/v1/ResumeGeneratorController.kt
@@ -0,0 +1,15 @@
+package com.experimentals.resume.generator.features.resume.generator.api.v1
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1")
+class ResumeGeneratorController {
+
+    @GetMapping("/test")
+    fun test(): String{
+        return "Hello, Resume Generator!"
+    }
+}
