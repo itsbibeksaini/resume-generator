323a62cb0db1a9f4f0b74b87827f9310b9c5b95b
Bibek Saini <itsbibeksaini@gmail.com>
2025-07-30 18:39:44 -0400

    Auth controller

---
+++ apps/server/src/main/kotlin/com/experimentals/resume/generator/features/authentication/api/v1/AuthController.kt
@@ -0,0 +1,15 @@
+package com.experimentals.resume.generator.features.authentication.api.v1
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/auth")
+class AuthController {
+
+    @PostMapping("/signin")
+    fun signIn(){
+        
+    }
+}
