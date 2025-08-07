893087d0591b7174570e9e203086b85283d1c210
Bibek Saini <itsbibeksaini@gmail.com>
2025-08-07 18:13:16 -0400

    update

--- apps/server/src/main/kotlin/com/experimentals/resume/generator/features/identity/data/responsemodels/AccountCreationResponse.kt
+++ apps/server/src/main/kotlin/com/experimentals/resume/generator/features/identity/data/responsemodels/AccountCreationResponse.kt
@@ -2,6 +2,7 @@ package com.experimentals.resume.generator.features.identity.data.responsemodels
 
 import io.swagger.v3.oas.annotations.media.Schema
 
+@Schema(description = "Response model returned for account creation request.")
 data class AccountCreationResponse(
 
     @Schema(description = "Status message", example = "Account creation successfully")
