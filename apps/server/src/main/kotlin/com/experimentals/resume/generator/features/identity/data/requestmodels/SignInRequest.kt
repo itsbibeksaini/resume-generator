341a674e386191a77fbf40577226e205b9db920b
Bibek Saini <itsbibeksaini@gmail.com>
2025-08-16 09:18:08 -0400

    fix: jackson wont load request body data

--- apps/server/src/main/kotlin/com/experimentals/resume/generator/features/identity/data/requestmodels/SignInRequest.kt
+++ apps/server/src/main/kotlin/com/experimentals/resume/generator/features/identity/data/requestmodels/SignInRequest.kt
@@ -1,8 +1,9 @@
 package com.experimentals.resume.generator.features.identity.data.requestmodels
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import jakarta.validation.constraints.NotBlank
 
-data class SignInRequest(
+data class SignInRequest @JsonCreator constructor (
     @NotBlank
 //    @Pattern(regexp = "[a-zA-Z0-9_]+")
     val username: String,
