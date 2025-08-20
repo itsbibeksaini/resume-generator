c0ab6a54161650c80ae50c3b048720f489e47382
Bibek Saini <itsbibeksaini@gmail.com>
2025-08-19 20:09:57 -0400

    update

---
+++ apps/client/src/core/routing/routes.ts
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import App from "../../App";
+
+export const ROUTES = createBrowserRouter([
+    {
+        path: '/',
+        Component: App
+    }
+    
+]);
