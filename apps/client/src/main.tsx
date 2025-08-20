52442db1eabe9bd82ec3583eebe5ff3f2e09a0e9
Bibek Saini <itsbibeksaini@gmail.com>
2025-08-19 20:17:09 -0400

    client app > vite adding embedded sass

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import { RouterProvider } from 'react-router'
 import { ROUTES } from './core/routing/routes.ts'
 
