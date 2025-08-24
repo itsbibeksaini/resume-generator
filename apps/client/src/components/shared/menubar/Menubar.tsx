40163b1d0c1923eb37796a3af418460498258978
Bibek Saini <itsbibeksaini@gmail.com>
2025-08-23 20:03:08 -0400

    update

--- apps/client/src/components/shared/menubar/Menubar.tsx
+++ apps/client/src/components/shared/menubar/Menubar.tsx
@@ -7,8 +7,8 @@ const Menubar: FC = () => {
     
   return (
     <Grid className={`${styles.menubar}`} container>
-        <Grid size={6}>
-            {/* <Typography variant="h4">Resume Generator</Typography> */}
+        <Grid size={6} sx={{position: 'relative'}}>
+            <Typography variant="h4" className="vertical-center">Resume Generator</Typography>
         </Grid>
         <Grid container size={6} sx={{justifyContent: 'flex-end'}}>
             <ChipAvatar name="John Doe" avatar="jd" position="Software Engineer" />
