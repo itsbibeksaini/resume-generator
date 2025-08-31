e3a483df499412ef46d2b3677c6b85667348669a
Bibek Saini <itsbibeksaini@gmail.com>
2025-08-30 23:04:10 -0400

    Sidebar

--- apps/client/src/components/shared/sidebar/Sidebar.tsx
+++ apps/client/src/components/shared/sidebar/Sidebar.tsx
@@ -1,8 +1,10 @@
-import {  ButtonBase, Grid } from "@mui/material";
+import {  Box, ButtonBase, Grid } from "@mui/material";
 import { useState } from "react";
 import styles from "./Sidebar.module.scss";
 import ChipAvatar from "../chip-avatar/ChipAvatar";
 import DropDownMenu from "./DropDownMenu/DropDownMenu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
     const [isExpanded, setIsExpanded] = useState(false);
@@ -11,30 +13,41 @@ const Sidebar = () => {
 
     const toggleSidebar = () => {
         setIsExpanded(!isExpanded);
-        showDropDown ? setShowDropDown(false) : null;
+        showDropDown ? setShowDropDown(false) : setShowDropDown(false);
     };
 
-    const toggleDropDown = () => {
+    const toggleDropDown = () => {        
         setShowDropDown((prev) => !prev);  
         handleTouch();      
     }
 
-    const handleTouch = () => {
+    const handleTouch = () => {        
         if (!isTouched) 
             setIsTouched(true);
-        
     };
 
     return (
         <>
-        <Grid className={`${styles.sidebar} ${isExpanded ? styles.expandSidebar : ''}`} container direction="column">
+        <Box className={`${styles.overlay} ${isExpanded ? styles.showOverlay : ''}`} onClick={toggleSidebar} />
+
+        <Grid className={`${styles.sidebar} ${isExpanded ? styles.expandSidebar : ''}`} container direction="column">            
+
             <Grid sx={{position:"absolute", bottom:10, left:0, right:0, }}>
-                <ButtonBase sx={{padding:'0.25rem 0.75rem', width:'100%', backgroundColor:'rgba(255,255,255,0.1)', '&:hover':{backgroundColor:'rgba(255,255,255,0.2)'}}}>
-                    <ChipAvatar name="John Doe" avatar="jd" position="Software Engineer" isExpanded={isExpanded} toggleSidebar={toggleSidebar} toggleDropDown={toggleDropDown} showDropDown={showDropDown} />
+                <ButtonBase sx={{padding:'0.25rem 0.75rem', width:'100%', backgroundColor:'rgba(255,255,255,0.1)', '&:hover':{backgroundColor:'rgba(255,255,255,0.2)'}}} >
+                    <ChipAvatar 
+                        name="John Doe" 
+                        avatar="jd" 
+                        position="Software Engineer" 
+                        isExpanded={isExpanded} 
+                        toggleSidebar={toggleSidebar} 
+                        toggleDropDown={toggleDropDown} 
+                        showDropDown={showDropDown}
+                    />
                 </ButtonBase>
                 <DropDownMenu showDropDown={showDropDown} isTouched={isTouched} />
             </Grid>
         </Grid>
+
         </>
     );
 };
