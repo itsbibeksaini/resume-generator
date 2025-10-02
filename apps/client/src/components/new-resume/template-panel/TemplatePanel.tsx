b2d2fc577400a5dba1adaac9d90546a2c6101a39
Bibek Saini <itsbibeksaini@gmail.com>
2025-10-02 10:57:58 -0400

    template pannel

--- apps/client/src/components/new-resume/template-panel/TemplatePanel.tsx
+++ apps/client/src/components/new-resume/template-panel/TemplatePanel.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, ButtonBase, Checkbox, Grid, Typography } from "@mui/material";
 import type { FC } from "react";
 import styles from './TemplatePanel.module.scss';
 
@@ -8,6 +8,25 @@ const TemplatePanel: FC = () => {
             <header>
                 <Typography variant="h5">Choose a template</Typography>
             </header>
+            <Grid className={`${styles.templates}`} container gap={3}>
+                <ButtonBase sx={{width:'100%'}}>
+                    <Box className={`${styles.template} ${styles.selected}`}>
+                        <Checkbox className={`${styles.checkbox}`}/>
+                        <Box className={`${styles.banner}`}>
+                            <Typography>Template 1</Typography>
+                        </Box>
+                    </Box>
+                </ButtonBase>
+
+                <ButtonBase sx={{width:'100%'}}>
+                    <Box className={`${styles.template}`}>
+                        <Checkbox className={`${styles.checkbox}`}/>
+                        <Box className={`${styles.banner}`}>
+                            <Typography>Template 1</Typography>
+                        </Box>
+                    </Box>
+                </ButtonBase>
+            </Grid>
         </Grid>
     );
 };
