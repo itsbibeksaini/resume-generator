e857b6597223f00f91c1b644488755d49b188df9
Bibek Saini <itsbibeksaini@gmail.com>
2025-11-07 20:47:50 -0500

    return memo for react element

--- apps/client/src/core/dynamic-fields/fields/core/DynamicField.ts
+++ apps/client/src/core/dynamic-fields/fields/core/DynamicField.ts
@@ -1,7 +1,8 @@
+import type { JSX, MemoExoticComponent } from "react";
 import type { FieldConfig } from "./FieldConfig";
 
 export const DynamicField = {
-    create: (config: FieldConfig, options: {render: () => string}) => {
+    create: (config: FieldConfig, options: {render: () => MemoExoticComponent<() => JSX.Element>}) => {
         return { render: options.render };
     }
 }
--- apps/client/src/core/dynamic-fields/fields/types/CustomTextField.tsx
+++ apps/client/src/core/dynamic-fields/fields/types/CustomTextField.tsx
@@ -1,7 +1,10 @@
+import { memo } from "react";
 import { DynamicField } from "../core/DynamicField";
 import type { FieldConfig } from "../core/FieldConfig";
 
 export const CustomTextField = (config: FieldConfig): DynamicField => 
     DynamicField.create(config, {
-        render: () => "Hello World"
+        render: () => memo(() => {
+            return <>Hello</>
+        })
     })
