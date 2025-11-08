86ec63b1de98b1918a64b8210d2b6383be961c59
Bibek Saini <itsbibeksaini@gmail.com>
2025-11-07 21:09:22 -0500

    adding ZodType schema to dynamic field

--- apps/client/src/core/dynamic-fields/fields/core/DynamicField.ts
+++ apps/client/src/core/dynamic-fields/fields/core/DynamicField.ts
@@ -1,9 +1,11 @@
 import type { JSX, MemoExoticComponent } from "react";
 import type { FieldConfig } from "./FieldConfig";
+import type z from "zod";
 
 export const DynamicField = {
-    create: (config: FieldConfig, options: {render: () => MemoExoticComponent<() => JSX.Element>}) => {
-        return { render: options.render };
+    create: (config: FieldConfig, options: {render: () => MemoExoticComponent<() => JSX.Element>, getSchema: () => z.ZodType}) => {
+        return { render: options.render, getSchema: options.getSchema
+         };
     }
 }
 
--- apps/client/src/core/dynamic-fields/fields/types/CustomTextField.tsx
+++ apps/client/src/core/dynamic-fields/fields/types/CustomTextField.tsx
@@ -1,10 +1,12 @@
 import { memo } from "react";
 import { DynamicField } from "../core/DynamicField";
 import type { FieldConfig } from "../core/FieldConfig";
+import z from "zod";
 
 export const CustomTextField = (config: FieldConfig): DynamicField => 
     DynamicField.create(config, {
         render: () => memo(() => {
             return <>Hello</>
-        })
+        }),
+        getSchema: () => z.string()
     })
