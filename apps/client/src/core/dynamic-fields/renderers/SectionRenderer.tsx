faf4d381576fe11d8a7468b8b2ef1ace154a1db7
Bibek Saini <itsbibeksaini@gmail.com>
2025-11-20 16:52:04 -0500

    v1

---
+++ apps/client/src/core/dynamic-fields/renderers/SectionRenderer.tsx
@@ -0,0 +1,81 @@
+import { useState, type FC } from "react"
+import type { FieldConfig } from "../core/FieldConfig"
+import { Box, Grid, Typography } from "@mui/material"
+import styles from '../core/RendererStyles.module.scss'
+import { FieldRenderer } from "./FieldRenderer"
+
+type Section = {
+    rows: SectionRow[]
+    header: string
+}
+
+type SectionRow = {
+  subSection: boolean
+  header?: string
+  fields: FieldConfig[]
+}
+
+type SectionRendererProps = {
+    section: Section,
+    hasError?: boolean
+}
+
+const SectionRenderer: FC<SectionRendererProps> = (props: SectionRendererProps) => {
+
+    const [values, setValues] = useState<Record<string, any>>(
+    // initialize from config values
+        props.section.rows.flatMap(row => row.fields).reduce((acc, field) => {
+        acc[field.name] = "";
+        return acc;
+        }, {} as Record<string, any>)
+    );
+    
+    return(
+        <Grid className={`${styles.section}`}>
+            <Box className={`${styles.errorBox} ${props.hasError ? styles.showError + ' shake' : ''}`}>
+                <Box>
+                    <Typography variant="h6">{props.section.header}</Typography>
+                </Box>
+                {
+                    props.section.rows.map((row, rowIndex) => {
+                        return(
+                            <Grid container className={`${styles.row}`} key={rowIndex}>
+                                {
+                                    <>
+                                        {row.subSection && 
+                                            <Grid size={12}>
+                                                <Typography variant="subtitle1">{row.header}</Typography>
+                                            </Grid>
+                                        }
+                                        {Array.isArray(row.fields) && row.fields.map((field, fieldIndex) => {
+                                            if (!field.type) return null;
+                                        field.errorText = "test"
+                                            return (
+                                                <Grid size={field.col} key={fieldIndex} className={`${styles.col}`}>
+                                                    {/* <FieldComponent 
+                                                        label={field.label} 
+                                                        id={field.id} 
+                                                        name={field.name} 
+                                                        placeholder={field.placeholder}
+                                                        col={0}                                                                    
+                                                        required={field.required}
+                                                        icon={field.icon}
+                                                        errorText={errors[field.name as keyof ResumeSectionInfo]}                                                            
+                                                        onBlur={(e: FocusEvent<Element>) => updateField(e, field.name)}
+                                                    /> */}
+                                                    <FieldRenderer config={field}/>
+                                                </Grid>
+                                            );
+                                        })}
+                                    </>
+                                }
+                            </Grid>
+                        )
+                    })
+                }
+            </Box>
+        </Grid>
+    )
+}
+
+export default SectionRenderer;
