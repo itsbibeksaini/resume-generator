bc79e5238e4a75ea746e6b318d3d0ad4a93a24c5
Bibek Saini <itsbibeksaini@gmail.com>
2025-11-21 20:49:25 -0500

    adding event

--- apps/client/src/core/dynamic-fields/sections/personal-details.ts
+++ apps/client/src/core/dynamic-fields/sections/personal-details.ts
@@ -3,41 +3,44 @@ import type { Section } from "../renderers/SectionRenderer";
 import z from "zod";
 
 export const PERSONAL_DETAILS: Section = {
-    header: 'Personal & Contact Information',
-    rows: [
+  header: 'Personal & Contact Information',
+  rows: [
+    {
+      subSection: false,
+      fields: [
         {
-        subSection: false,
-        fields: [
-          {
-            id:'txt-firstname',
-            name:'firstName',
-            type: "text",
-            label:'First Name',
-            placeholder: 'Type your first name, e.g., John',
-            icon: faUser,
-            col: 4,
-            events: [{
-                type: 'blur'
-            }],
-            required:true,
-             validations: z.string().trim()
-                            .min(2, {message: "First name must be at least 2 characters long"})
-                            .max(50, {message: "First name must be at most 50 characters long"})
-            
-          }, {
-            id:'txt-lastname',
-            name:'lastName',
-            type: "text",
-            label:'Last Name',
-            placeholder:"Type your last name, e.g., Doe",
-            icon: faUser,
-            col: 4,
-            required:true,
-            validations: z.string().trim()
-                            .min(2, {message: "Last name must be at least 2 characters long"})
-                            .max(50, {message: "Last name must be at most 50 characters long"})
-          }
-        ]
-      }
-    ]
+          id: 'txt-firstname',
+          name: 'firstName',
+          type: "text",
+          label: 'First Name',
+          placeholder: 'Type your first name, e.g., John',
+          icon: faUser,
+          col: 4,
+          events: [{
+            type: 'blur'
+          }],
+          required: true,
+          validations: z.string().trim()
+            .min(2, { message: "First name must be at least 2 characters long" })
+            .max(50, { message: "First name must be at most 50 characters long" })
+
+        }, {
+          id: 'txt-lastname',
+          name: 'lastName',
+          type: "text",
+          label: 'Last Name',
+          placeholder: "Type your last name, e.g., Doe",
+          icon: faUser,
+          col: 4,
+          required: true,
+          events: [{
+            type: 'blur'
+          }],
+          validations: z.string().trim()
+            .min(2, { message: "Last name must be at least 2 characters long" })
+            .max(50, { message: "Last name must be at most 50 characters long" })
+        }
+      ]
+    }
+  ]
 }
