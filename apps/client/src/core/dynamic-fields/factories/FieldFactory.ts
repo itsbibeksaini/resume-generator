1209fff0af0336d4acab420a9309aa406664e8b2
Bibek Saini <itsbibeksaini@gmail.com>
2025-11-26 23:20:46 -0500

    custom date picker

--- apps/client/src/core/dynamic-fields/factories/FieldFactory.ts
+++ apps/client/src/core/dynamic-fields/factories/FieldFactory.ts
@@ -1,20 +1,21 @@
 import type { JSX } from "react"
 import type { FieldConfig } from "../core/FieldConfig"
 import CustomTextField from "../fields/CustomTextField"
+import CustomDatePicker from "../fields/CustomDatePicker"
 
 type FieldFactory = {
-    createField:(config: FieldConfig) => JSX.Element
+    createField: (config: FieldConfig) => JSX.Element
 }
 
 export const FieldFactoryImpl: FieldFactory = {
     createField: (config: FieldConfig) => {
-       switch(config.type) {
+        switch (config.type) {
             case "text":
                 return CustomTextField.create(config).render();
-            // case "date-picker":
-            //     return DatePickerField.create(config).render();
+            case "date-picker":
+                return CustomDatePicker.create(config).render();
             default:
                 throw new Error(`Unsupported field type: ${config.type}`);
-         }
+        }
     }
 }
---
+++ apps/client/src/core/dynamic-fields/fields/CustomDatePicker.tsx
@@ -0,0 +1,96 @@
+import { Box, Grid, InputAdornment, Typography } from "@mui/material";
+import type { DynamicField } from "../core/DynamicField";
+import type { FieldConfig } from "../core/FieldConfig";
+import styles from '../core/FieldStyles.module.scss'
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+
+const CustomDatePicker: DynamicField = {
+    create: (config: FieldConfig) => {
+        return {
+            render: () => {
+                const showError = Boolean(config.errorText && config.errorText.trim().length > 0);
+                return (
+                    <Box className={`${styles.fieldWrapper} ${showError ? styles.showError + ' shake' : ''}`}>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker
+                                sx={{ width: '100%' }}
+                                views={["year", "month"]}
+                                format="MM/YYYY"
+                                label={config.label}
+                                name={config.name}
+                                {
+                                ...
+                                (config.events?.reduce((acc, event) => {
+                                    if (event.type === 'blur' && event.handler)
+                                        acc.onBlur = event.handler;
+                                    if (event.type === 'change' && event.handler)
+                                        acc.onChange = event.handler;
+                                    return acc;
+                                }, {} as Record<string, (evt: React.FocusEvent) => void>) || {})
+                                }
+                                slotProps={{
+                                    openPickerIcon: {
+                                        sx: {
+                                            color: showError ? "#212121" : ""
+                                        }
+                                    },
+                                    textField: {
+                                        id: config.id,
+                                        required: config.required,
+                                        helperText: config.helperText,
+                                        error: showError,
+                                        sx: {
+                                            zIndex: '1',
+                                            // Stop browser autofill coloration
+                                            '& input:-webkit-autofill': {
+                                                WebkitBoxShadow: '0 0 0 1000px white inset !important',
+                                                WebkitTextFillColor: '#000 !important',
+                                            },
+                                            '& input:-webkit-autofill:focus': {
+                                                WebkitBoxShadow: '0 0 0 1000px white inset !important',
+                                                WebkitTextFillColor: '#000 !important',
+                                            },
+                                        },
+                                        slotProps: {
+                                            input: {
+                                                ...
+                                                (
+                                                    config.icon ? {
+                                                        startAdornment: (
+                                                            <InputAdornment position='start'>
+                                                                <FontAwesomeIcon icon={config.icon} style={{ color: showError ? "#212121" : "" }} />
+                                                            </InputAdornment>
+                                                        )
+                                                    } : {}
+                                                ),
+                                                sx: {
+                                                    color: showError ? "#212121" : ""
+                                                },
+                                                title: config.placeholder
+                                            }
+                                        }
+                                    }
+
+                                }}
+                            />
+                        </LocalizationProvider>
+                        <Grid container className={`${styles.errorTextWrapper} ${showError ? styles.showErrorText : ''}`} gap={0.5}>
+                            <Grid size='auto' sx={{ paddingTop: '0.2rem' }}>
+                                <FontAwesomeIcon icon={faCircleExclamation} className={`${styles.errorIcon}`} />
+                            </Grid>
+                            <Grid size='grow' className={`${styles.errorText}`}>
+                                <Typography variant="caption" color="error">{config.errorText}</Typography>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                )
+            }
+        }
+    }
+}
+
+export default CustomDatePicker;
